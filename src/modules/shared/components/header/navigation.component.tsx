2ef48a6d9f9b724caa35032d3552bb4389b281a0
James Hill <jahill@groupon.com>
2021-10-15 13:25:24 +0100

    Added navigation menu overlay

--- src/modules/shared/components/header/navigation.component.tsx
+++ src/modules/shared/components/header/navigation.component.tsx
@@ -15,17 +15,14 @@ function Navigation() {
   return (
     <Fragment>
       <button class={`${style.button} ${activeClass}`} onClick={onClickToggle}>
-        <svg
-          viewBox="0 0 100 100"
-          xmlns="http://www.w3.org/2000/svg"
-          preserveAspectRatio="xMinYMin meet"
-        >
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
           <circle cx="50" cy="50" r="30" />
           <path d="M0 40h62c13 0 6 28-4 18L35 35" />
           <path d="M0 50h70" />
           <path d="M0 60h62c13 0 6-28-4-18L35 65" />
         </svg>
       </button>
+      {isActive && <menu class={style.menu}>Navigation Menu..</menu>}
     </Fragment>
   );
 

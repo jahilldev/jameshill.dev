6b88248c3089b95f52c0321e52302fcb954ff724
James Hill <jahill@groupon.com>
2022-02-09 13:06:08 +0000

    Updated twitter username

--- src/modules/shared/components/socialMenu/socialMenu.component.tsx
+++ src/modules/shared/components/socialMenu/socialMenu.component.tsx
@@ -31,7 +31,7 @@ function SocialMenu({ className = '' }: IProps) {
   return (
     <nav class={`${style.social} ${className}`}>
       <a
-        href="https://twitter.com/jahill_dev"
+        href="https://twitter.com/jahilldev"
         target="_blank"
         rel="noreferrer"
         class={style.link}
